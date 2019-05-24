7e6c9c38008bd7cef4db97117b15744be8b01c7d
Sowon Park <thdnjs957@gmail.com>
2019-05-24 21:30:14 +0900

    added

--- src/main/java/com/cafe24/mysite/service/BoardService.java
+++ src/main/java/com/cafe24/mysite/service/BoardService.java
@@ -23,22 +23,41 @@ public class BoardService {
     public Map<String,Object> getList(String keyword,int curPage) { //map.put("keyword", keyword) , map.put("curPage",curPage);
        
        
-       int pageStart = (curPage-1) * BOARD_CNT + 1;
-       int pageEnd = pageStart+BOARD_CNT-1;
-       int totalCount = boardDao.getCount(keyword); 
-       int totalBlock = (int)Math.ceil(totalCount)/PAGE_CNT;
+    	//1. 페이징을 위한 기본 데이터 계산
+		int totalCount = boardDao.getCount( keyword ); 
+		int pageCount = (int)Math.ceil( (double)totalCount / BOARD_CNT );
+		int blockCount = (int)Math.ceil( (double)pageCount / PAGE_CNT );
+		int currentBlock = (int)Math.ceil( (double)curPage / PAGE_CNT );
+		
+		//2. 파라미터 page 값  검증
+		if( curPage > pageCount ) {
+			curPage = pageCount;
+			currentBlock = (int)Math.ceil( (double)curPage / PAGE_CNT );
+		}		
+		
+		if( curPage < 1 ) {
+			curPage = 1;
+			currentBlock = 1;
+		}
+		
+		//3. view에서 페이지 리스트를 렌더링 하기위한 데이터 값 계산
+		int beginPage = currentBlock == 0 ? 1 : (currentBlock - 1)*PAGE_CNT + 1;
+		int prevPage = ( currentBlock > 1 ) ? ( currentBlock - 1 ) * PAGE_CNT : 0;
+		int nextPage = ( currentBlock < blockCount ) ? currentBlock * PAGE_CNT + 1 : 0;
+		int endPage = ( nextPage > 0 ) ? ( beginPage - 1 ) + BOARD_CNT : pageCount;
        
        //boolean prev = pageStart != 1 ? true:false; // 페이징 이전 버튼 활성화 여부
        //boolean next = pageEnd * BOARD_CNT >= totalCount ? false : true;
        Map<String, Integer> pagerMap = new HashMap<String, Integer>();
-       pagerMap.put("pageStart", pageStart);
-       pagerMap.put("pageEnd", pageEnd);
+       pagerMap.put("pageStart", beginPage);
+//       pagerMap.put("pageEnd", pageEnd);
        
        
        Map<String, Object> listMap = new HashMap<String, Object>();
-       listMap.put("pageStart",pageStart);
+       listMap.put("curPage",curPage);
        listMap.put("pageCnt",PAGE_CNT);
        listMap.put("keyword", keyword);
+       
 
        List<BoardVo> list = boardDao.getList(listMap);
        
